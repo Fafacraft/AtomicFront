b659e14557480eeff356f8c86b009b33a3c42fc5
FabienArthurYnov <fabien.arthur@ynov.com>
2026-03-06 10:37:33 +0100

    fix case sensitive import

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./Navbar.css";
+import "./navbar.css";
 import { LoginModal } from "../LoginModal/LoginModal";
 import { env } from "../../config/env.js";
 import { verifyAuth } from "../../contexts/UserHelper.js";
